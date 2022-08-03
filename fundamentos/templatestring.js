32e34c47c12160f9cc6508eef7b7c0cda2936f52
Henrique Martins da Hora <henrique@martinsdahora.com.br>
2022-08-03 10:35:52 -0300

    Create templatestring.js

---
+++ fundamentos/templatestring.js
@@ -0,0 +1,9 @@
+console.clear()
+const nome = 'Rebeca'
+const concatenacao = 'Ola ' + nome + '!' // Forma 1 de Concatenar
+const template = `Olá ${nome}!` // Forma 2 de Concatenar.
+console.log(concatenacao)
+console.log(template)
+console.log(`1 + 1 = ${1+1}`)
+const up = texto => texto.toUpperCase()
+console.log(`EI... ${up('cuidado')}!`)
