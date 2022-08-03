059e89303aef83666092c35dffbb6ad26a8fa349
Henrique Martins da Hora <henrique@martinsdahora.com.br>
2022-08-02 21:54:01 -0300

    Create numerosAlgunsCuidados.js

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.clear()
+
+console.log(7 / 0) // Tipo Infinity.
+console.log("10" / 2) // Operações com o Tipo String que tenha números.
+console.log("show!" * 2) // NaN
+console.log(0.1 + 0.7) // Não é preciso...
+console.log((10.345).toFixed(2))
