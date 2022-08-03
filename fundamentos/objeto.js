3b6c692150f7c44be663851797db7ac47400bccd
Henrique Martins da Hora <henrique@martinsdahora.com.br>
2022-08-03 12:13:14 -0300

    Create objeto.js

---
+++ fundamentos/objeto.js
@@ -0,0 +1,12 @@
+console.clear()
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+prod1['desconto legal'] = 0.40 // Evite atributos com espaço.
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90
+}
+console.log(prod2)
